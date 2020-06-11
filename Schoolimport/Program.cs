08b932125145066c8d9c02185a7131c4974a3d1f
Anders Dahlgren <Anders.Dahlgren@tillvaxtverket.se>
2020-06-11 11:05:20 +0200

    project start...

---
+++ Schoolimport/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Schoolimport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
